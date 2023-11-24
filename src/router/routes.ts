a1a295c4dfb8bf06ab851bca23c7460653fe0193
viet19112002 <105862132+viet19112002@users.noreply.github.com>
2023-11-24 21:57:57 +0700

    a

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import {userController} from "../controllers/User.controller";
 
 let router = express.Router();
 
 const initAPIRoutes = (app) => {
-    router.get("/getAllNotes", noteController.getAllNotes);
+    router.get("/getAllNotes", userController.getAllNotes);
     return app.use("/api", router);
 }
 
